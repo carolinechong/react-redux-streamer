e797ee2df5156b098200b14b953735441a1d6561
Caroline <carolinechong0720@gmail.com>
2019-04-30 11:03:59 -0700

    Live streaming from OBS via flv.js

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,52 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  // Create video element reference to pass into JSX and video player
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  // When component first renders
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    // Deconstructed to avoid repetition
+    const { id } = this.props.match.params;
+
+    // Call Action Creator with this.props.match.params.id to fetch stream
+    this.props.fetchStream(id);
+    // Attempt to build the player
+    this.buildPlayer();
+  }
+  // If component successfully fetches the stream sometime in the future, call build player
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  // Clean up resources
+  componentWillUnmount() {
+    // Tell player to stop attempting to stream video and detach itself from video element inside render method.
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    // If we already built the player OR the stream does not yet exist
+    if (this.player || !this.props.stream) {
+      // Not going to try to build the player
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    // Otherwise, create video player. Assign to var "this.player"
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -16,6 +58,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
